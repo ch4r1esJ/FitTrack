3096f7781f506790b06c2af4b1d5e2ac5a4fd75c
ch4r1esJ <charles.janjgava.git@gmail.com>
2026-01-03 23:33:33 +0400

    First commit

---
+++ FitTrack/FitTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitTrack
+//
+//  Created by Charles Janjgava on 1/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
