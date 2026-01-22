be799877348a78f143073b1938cb32004c8d888b
ch4r1esJ <charles.janjgava.git@gmail.com>
2026-01-22 09:38:46 +0400

    add profile

---
+++ FitTrack/FitTrack/Presentation/Profile/FitnessProfileButton.swift
@@ -0,0 +1,29 @@
+//
+//  FitnessProfileButton.swift
+//  FitTrack
+//
+//  Created by Charles Janjgava on 1/22/26.
+//
+
+import SwiftUI
+
+struct FitnessProfileButton: View {
+    @State var title: String
+    @State var image: String
+    @State var action: (() -> Void)
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            HStack {
+                Image(systemName: image)
+                
+                Text(title)
+            }
+            .foregroundColor(.primary)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
--- FitTrack/FitTrack/Presentation/Profile/ProfileView.swift
+++ FitTrack/FitTrack/Presentation/Profile/ProfileView.swift
@@ -12,6 +12,59 @@ struct ProfileView: View {
     
     var body: some View {
         VStack {
+            HStack(spacing: 16) {
+                Image("avatar1")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .padding(.all, 8)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(.gray.opacity(0.25))
+                    )
+                
+                VStack(alignment: .leading) {
+                    Text("Good morning,")
+                        .font(.largeTitle)
+                        .foregroundColor(.gray)
+                    
+                    Text("Name")
+                        .font(.title)
+                }
+            }
+            
+            VStack {
+                FitnessProfileButton(title: "Edit name", image: "square.and.pencil") {
+                    print("name")
+                }
+                
+                FitnessProfileButton(title: "Edit image", image: "square.and.pencil") {
+                    print("image")
+                }
+            }
+            .background {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.gray.opacity(0.15))
+            }
+            
+            VStack {
+                FitnessProfileButton(title: "Contact us", image: "envelope") {
+                    print("name")
+                }
+                
+                FitnessProfileButton(title: "Privacy policy", image: "doc") {
+                    print("image")
+                }
+                
+                FitnessProfileButton(title: "Terms of Service", image: "doc") {
+                    print("image")
+                }
+            }
+            .background {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.gray.opacity(0.15))
+            }
+            
             if let errorMessage = viewModel.errorMessage {
                 Text(errorMessage)
                     .foregroundStyle(.red)
@@ -23,8 +76,12 @@ struct ProfileView: View {
                 title: "Log Out",
                 isVisible: true
             ) {
-                    viewModel.logoutTapped()
-                }
+                viewModel.logoutTapped()
+            }
         }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment:
+                .topLeading)
     }
 }
+
